34c1a71c5118df4246dcc31f5c8aee5521493a98
cvarga2125 <cvarga2@ivytech.edu>
2024-09-12 23:31:49 -0400

    Implemented Grid Layout
    Tile Distribution Logic
    Custom Tile Assets (They're Bad.)

---
+++ Assets/TileReference.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+
+public class TileReference : MonoBehaviour
+{
+    public GameObject AccentSquareRenderer;
+    public GameObject SquareRenderer;
+    public TMPro.TextMeshProUGUI Text;
+    public string TileType;
+}
